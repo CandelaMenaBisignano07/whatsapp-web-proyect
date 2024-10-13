a0f71239ec78ea001c9a47b954e9bfe927038294
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-13 20:33:09 -0300

    refactor: moved the contact functions from backend\services\messages.service.js

---
+++ backend/services/contacts.service.js
@@ -0,0 +1,37 @@
+import {mediaCharge, __dirname} from "../utils/utils.js";
+import fs from 'fs/promises'
+import { client } from "../configs/client.js"
+const getAllContactsService = async()=>{  ////esto iria en users cuando tenga la autenticacion
+    const contacts = await client.getChats();
+    return contacts;
+};
+const getOneContactService = async(id)=>{  ////esto iria en users cuando tenga la autenticacion
+    await fs.rm(`${__dirname}../../../frontend/public/media`, {recursive:true, force:true})
+    await fs.mkdir(`${__dirname}../../../frontend/public/media`)
+    const contact = await client.getChatById(id)
+    const contactNew = {isGroup: contact.isGroup, id: contact.id, name:contact.name}
+    const messages = await contact.fetchMessages({limit:5})
+    for(let message of messages){ //recorre los msjs obtenidos
+        const contactProfile = await message.getContact()
+        message.profilePicture =  await getProfilePictureService(contactProfile.id._serialized)
+        message.name = contactProfile.name ? contactProfile.name : contactProfile.id.user
+        if(message.hasMedia){ //si el msj tiene media
+            message.pathToMedia = await mediaCharge(message)
+        }
+    }
+    return {...contactNew, messages:messages.map((msg)=>{
+        if(msg.pathToMedia) return {pathToMedia: msg.pathToMedia, id:msg.id, hasMedia:msg.hasMedia, body:msg.body, type:msg.type, created_at: msg.timestamp, author:msg.name, profilePicture: msg.profilePicture ? msg.profilePicture : './img/whatsapp_default_image.jpeg'}
+        else return {pathToMedia: msg.pathToMedia, id:msg.id, hasMedia:msg.hasMedia, body:msg.body, type:msg.type,created_at: msg.timestamp, author: msg.name,  profilePicture: msg.profilePicture ? msg.profilePicture : './img/whatsapp_default_image.jpeg'}
+    })};
+};
+
+const getProfilePictureService = async(id)=>{
+    const profilePicture = await client.getProfilePicUrl(id);
+    return profilePicture
+};
+
+export{
+    getProfilePictureService,
+    getOneContactService,
+    getAllContactsService
+}

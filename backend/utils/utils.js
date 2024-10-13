81946fc642250e7fa39c0e296fc47e54473159a2
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-13 18:50:21 -0300

    feat: added function qrChange

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -2,11 +2,14 @@ import { v4 } from "uuid";
 import fs from 'fs/promises';
 import path from "path";
 import { fileURLToPath } from "url";
+import qrcode from 'qrcode';
+
 
 export const idGenerator = ()=>{
     const id = v4();
     return id;
 };
+
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -19,3 +22,10 @@ export const mediaCharge=async(message)=>{
     await fs.writeFile(`${__dirname}../../../frontend/public/media/${id}.${mediaType[1].split(';')[0]}`, data) //escribe un archivo en la carpeta indicada con la media
     return `/media/${id}.${mediaType[1].split(';')[0]}`
 }
+export const qrCharge=async(qr)=>{
+    const qrUrl= await qrcode.toDataURL(qr);
+    const qrBase64 = Buffer.from(qrUrl.split(',')[1], 'base64')
+    const id = idGenerator()
+    await fs.writeFile(`${__dirname}../../../frontend/public/media/${id}.${qrUrl.split(';')[0].split('/')[1]}`, qrBase64)
+    return `/media/${id}.${qrUrl.split(';')[0].split('/')[1]}`
+};

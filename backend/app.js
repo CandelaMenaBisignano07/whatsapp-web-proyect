589d5e8c31e6baf21270223b39815fe51577a4ca
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-13 17:21:06 -0300

    removed console.logs and added a variable for websockets

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 import express from "express"
 import messagesRouter from "./routes/messages.router.js"
+import contactsRouter from './routes/contacts.router.js'
 import mongoose from "mongoose";
 import cors from 'cors'
 import { configs } from "./configs.js";
+import { Server } from "socket.io";
+import { client } from "./configs/client.js";
 const app = express()
 app.use(express.json());
 app.use(cors({
@@ -10,11 +13,16 @@ app.use(cors({
 }))
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/messages', messagesRouter)
+app.use('/api/contacts', contactsRouter)
 try {
-    await mongoose.connect(configs.mongoUrl)
-    console.log('conectado a la BDD')
+    await mongoose.connect(configs.mongoUrlTest)
+    client.initialize()
 } catch (error) {
     console.log(error.message)
 }
 
-app.listen(configs.port, ()=> console.log('connected'))
+const server = app.listen(configs.port, ()=> console.log('connected'))
+
+export const socketServer = new Server(server, {
+    cors: 'http://localhost:3000'
+})

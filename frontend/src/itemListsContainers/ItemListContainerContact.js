59b0adf179a0e3dfc41de2938abc62655e09db43
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-13 21:56:04 -0300

    feat and refactor: moved itemListContainer from frontend/src/pages/ContactPage.js and added listeners

---
+++ frontend/src/itemListsContainers/ItemListContainerContact.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { ClientContext } from '../context/ClientContext'
+import Loader from '../components/Loader'
+import { useParams } from 'react-router-dom'
+import ItemListContacts from '../itemLists/ItemListContacts'
+import SideBar from '../components/SideBar'
+import { fetchContact } from '../api/contactsApi'
+const ItemListContainerContacts = () => {
+    const {contactId} = useParams()
+    const [contact, setContact] = useState(null)
+    const [loader, setLoader] = useState(true)
+    const {socket} = useContext(ClientContext)
+    useEffect(()=>{
+        const abortController = new AbortController()
+        const signal = abortController.signal
+        
+        socket.on('messageRecieved', (client_message)=>{
+            if(client_message.from === contactId) fetchContact(signal, contactId, setContact)
+        })
+
+        socket.on('messageSended', (client_message)=>{
+            if(client_message.to === contactId) fetchContact(signal, contactId, setContact)
+        })
+
+        socket.on('revokedMessage', (revoked_message)=>{
+            console.log('aquii')
+            if(revoked_message.to == contactId) fetchContact(abortController.signal, contactId, setContact)
+        })
+
+        fetchContact(signal, contactId, setContact)
+        setLoader(false)
+        return()=>{
+            socket.off('messageRecieved')
+            socket.off('messageSended')
+            abortController.abort()
+        };
+        
+    }, [contactId])
+    if(!contact) return <Loader/>  // falta el loader con el estado
+  return (
+    <div className='containerGrid'>
+        <SideBar/>
+        <section className='contactContainer'>
+            <header className='contactHeader'>
+                <img src={contact.profile_picture}/>
+                <h2>{contact.name}</h2>
+            </header>
+            <section className='messagesContactContainerWrapper'>
+                <ItemListContacts data={contact}/>
+            </section>
+        </section>
+    </div>
+  )
+}
+
+export default ItemListContainerContacts

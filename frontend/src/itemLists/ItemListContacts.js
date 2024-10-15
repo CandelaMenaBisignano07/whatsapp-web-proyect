735e790aa2cd407b6815afe233584825fe3a3b7d
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-14 23:47:04 -0300

    fix: added function dateDifference to manage delete message button logic

--- frontend/src/itemLists/ItemListContacts.js
+++ frontend/src/itemLists/ItemListContacts.js
@@ -1,17 +1,28 @@
 import { useContext} from 'react'
 import Button from '../components/Button'
 import ItemMessagesContact from '../Items/ItemMessagesContact'
-import { ClientContext } from '../context/ClientContext'
+import { ClientContext } from '../context/ClientContext';
 const ItemListContacts = ({data}) => {
+    const dateDifference = (message)=>{
+        const TIME_LIMIT_MS = (23 * 60 * 60 + 58 * 60 + 0) * 1000;
+        console.log(TIME_LIMIT_MS)
+        const now = Date.now();
+        const messageTimestampMs = message.created_at * 1000;
+        const timeDifference = now - messageTimestampMs;
+        return timeDifference <= TIME_LIMIT_MS ? true : false
+    }
     const {URL} = useContext(ClientContext);
     const eliminateMessage = async(id)=>{
         try {
-            await fetch(`${URL}messages/${id}`,{
+            const hola = await fetch(`${URL}messages/${id}`,{
                 method:'DELETE', 
                 headers:{
                     'Content-Type': 'application/json'
                 }
             })
+
+            const json = await hola.json()
+            console.log(json)
         } catch (error) {
             console.log(error.message)
         }
@@ -26,7 +37,9 @@ const ItemListContacts = ({data}) => {
                             msg.id.fromMe && msg.type != 'revoked' ? (
                             <div className='containerMessageContact' key={msg.id.id}>
                                 <div className='messageContact'>
-                                    <Button body={'eliminar'} callback={eliminateMessage} id={msg.id._serialized}/>
+                                    {
+                                        dateDifference(msg) ? <Button body={'eliminar'} callback={eliminateMessage} id={msg.id._serialized}/> : null
+                                    }
                                     <ItemMessagesContact message={msg}/>
                                 </div>
                             </div>

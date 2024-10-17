b48303d2d7b5aecacf7fd04311b976fef6e96614
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-16 22:54:51 -0300

    feat: added body prop to loader component

--- frontend/src/itemLists/ItemListMessages.js
+++ frontend/src/itemLists/ItemListMessages.js
@@ -2,7 +2,9 @@ import ItemMessage from '../Items/ItemMessage'
 import Loader from '../components/Loader'
 const ItemListMessages = ({data, isLoadingMessages, isLoading}) => {
     console.log(isLoadingMessages, 'desde itemlist')
-    if(isLoadingMessages || isLoading) return <Loader/>
+    if(isLoadingMessages) return <Loader body={'awaiting for a programmed message to be sent'}/>
+
+    if(isLoading) return <Loader body={'awaiting messages'}/>
     return (
     <>
         <ul className="containerMessages">

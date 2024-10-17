73b46cf094828a8a621ae538abe09f8abf3e5ec7
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-16 22:50:03 -0300

    feat: added route /error/:code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import QrPage from "./pages/QrPage.js";
 import ClientProvider from "./context/ClientContext.js";
 import MessagesPage from "./itemListsContainers/ItemListContainerMessages.js";
 import ItemListContainerContacts from "./itemListsContainers/ItemListContainerContact.js";
+import Error from './pages/error/Error.js';
 /// quiero hacer un coso de whatsapp en donde en el inicio aparezcan los mensajes recientemente enviados y que tambien de la opcion de chatbot automatico
 /// tambien quiero un sidebar donde esten todos los contactos y se pueda ingresar a su contacto al chat (solo que muestre lo que nosotros enviamos) y podamos enviar mensajes desde ahi controlando el tiempo
 /// ademas de que los mensajes tambien se puedan borrar, editar y buscar por fecha y por nombre.
@@ -10,16 +11,17 @@ import ItemListContainerContacts from "./itemListsContainers/ItemListContainerCo
 function App() {
   return (
     <div className="App">
-      <ClientProvider>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="/" element={<QrPage/>}/>
-            <Route path={"/:contactId"} element={<ItemListContainerContacts/>}/>
-            <Route path={"/home"} element={<MessagesPage/>} />
-          </Routes>
-        </BrowserRouter>
-      </ClientProvider>
-    </div>
+        <ClientProvider>
+          <BrowserRouter basename="/">
+            <Routes>
+              <Route path="/" element={<QrPage/>}/>
+              <Route path={"/:contactId"} element={<ItemListContainerContacts/>}/>
+              <Route path={"/home"} element={<MessagesPage/>} />
+              <Route path={"/error/:code"} element={<Error/>}/>
+            </Routes>
+          </BrowserRouter>
+        </ClientProvider>
+      </div>
   );
 }
 

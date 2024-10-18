b714e2da0687ba23d882dc7536cf40e84b9ef1a4
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-17 23:37:12 -0300

    feat: added error handling logic at catch block for error rendering

--- frontend/src/api/contactsApi.js
+++ frontend/src/api/contactsApi.js
@@ -1,6 +1,6 @@
-
 const URL = 'http://localhost:8080/api/contacts';
-export const fetchContact = async(signal,contactId, setContact)=>{
+export const fetchContact = async(signal,contactId, setContact, callback, setError)=>{
+    
     try {
         console.log('hola estoy llamando 2 veces')
         const contactFetch = await fetch(`${URL}/${contactId}`, {
@@ -10,6 +10,11 @@ export const fetchContact = async(signal,contactId, setContact)=>{
             },
             signal: signal
         });
+        if(contactFetch.status != 200 ) {
+            const {error: errorMessage} = await contactFetch.json()
+            setError({code:contactFetch.status, message: errorMessage})
+            return callback(`/error/${contactFetch.status}`)
+        }
         const {payload: contactFetched} = await contactFetch.json()
         console.log(contactFetched)
         const profilePictureFetch = await fetch(`${URL}/profile-picture/${contactFetched.id._serialized}`,{
@@ -19,9 +24,20 @@ export const fetchContact = async(signal,contactId, setContact)=>{
             },
             signal:signal
         })
+
+        if(profilePictureFetch.status != 200 ) {
+            const {error: errorMessage} = await profilePictureFetch.json()
+            setError({code:profilePictureFetch.status, message: errorMessage})
+            return callback(`/error/${profilePictureFetch.status}`)
+        }
         const {payload: profilePicture} = await profilePictureFetch.json()
         return profilePicture === undefined ? setContact({...contactFetched, profile_picture:'./img/whatsapp_default_image.jpeg'}) : setContact({...contactFetched, profile_picture: profilePicture}) 
     } catch (error) {
-        console.log(error.message)
+        if(error.message === 'signal is aborted without reason'){
+            return
+        }  
+        else console.log(error.message)
     }
 }
+
+//faltan las otras peticiones

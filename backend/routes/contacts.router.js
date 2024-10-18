568ce65d1f31765a7757fe9c498f2ea3c7ab721a
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-17 23:15:46 -0300

    feat: added route for destroyClient feature

--- backend/routes/contacts.router.js
+++ backend/routes/contacts.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getProfilePicture, getAllContacts, getOneContact} from "../controllers/contacts.controller.js";
+import { getProfilePicture, getAllContacts, getOneContact, destroyClient} from "../controllers/contacts.controller.js";
 import { handleClient } from "../middlewares/middlewares.js";
 const router = Router();
 
 router.get('/',handleClient, getAllContacts);
 router.get('/:id', handleClient, getOneContact);
 router.get('/profile-picture/:id', handleClient, getProfilePicture);
+router.delete('/', handleClient, destroyClient);
 
 export default router

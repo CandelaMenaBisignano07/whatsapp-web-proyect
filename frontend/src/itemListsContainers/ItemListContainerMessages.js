46e23833dabfb17a53e21876bb77c6efaf79a34d
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-21 22:56:53 -0300

    fix, refactor and feat: added api constant and verifyNumber function imports, changed fetchMessages function location to src/lib/api/api.js, added logic to validate users number at changeState function and added params prop to button component

--- frontend/src/itemListsContainers/ItemListContainerMessages.js
+++ frontend/src/itemListsContainers/ItemListContainerMessages.js
@@ -4,61 +4,34 @@ import { ClientContext } from "../context/ClientContext"
 import InputMessages from "../components/InputMessages"
 import Button from '../components/Button'
 import ItemListMessages from "../itemLists/ItemListMessages"
+import { api } from "../lib/api/api"
+import { verifyNumber } from '../utils/utils'
 const MessagesPage = () => {
     const [data, setData] = useState([])
     const [inputData, setInputData] = useState({})
     const [isLoading, setIsLoading] = useState(true) // estado loader fetch general
-    const {URL, client, localStorageIsLoading, setLocalStorageIsLoading, setError} = useContext(ClientContext) // estado loader de mensajes con delay
+    const {client, localStorageIsLoading, setLocalStorageIsLoading, setError} = useContext(ClientContext) // estado loader de mensajes con delay
     const inputChange = (name, data)=>{
         const newOne = structuredClone(inputData)
         newOne[name] = data;
         setInputData(newOne)
     }
-    const fetchMessages = async()=>{
-        try {
-            const messages = await fetch(`${URL}messages`, {
-                method:'GET',
-                headers:{
-                    'Content-Type': "application/json"
-                }
-            });
-
-            const {payload} = await messages.json();
-            setData(payload.filter(m => m.from === client.me._serialized))
-        } catch (error) {
-            if(error.message === 'signal is aborted without reason'){
-                return
-            }  
-            else console.log(error.message)
-        }
-    }
     useEffect(()=>{
         console.log(localStorageIsLoading, 'desde el useeffect')
         if(!localStorageIsLoading){
-            fetchMessages()
+            api.messages.fetchMessages(client, setData)
             setIsLoading(false)
         }
     }, [localStorageIsLoading])
 
-    const changeState=async()=>{
-        if (inputData.message && inputData.seconds && inputData.number){
+    const changeState=async()=>{ 
+        if (inputData.message && inputData.seconds && inputData.number){ 
+            if(!verifyNumber(inputData.number)) return alert('invalid number, please write a valid number')
             localStorage.setItem('loadingMessage', JSON.stringify(true))
             setLocalStorageIsLoading(JSON.parse(localStorage.getItem('loadingMessage')))
             const timer = setTimeout(async()=>{
                 try{
-                    const promise = await fetch(`${URL}messages`, {
-                        method:'POST',
-                        body: JSON.stringify({
-                            description: inputData.message,
-                            send_at: inputData.seconds,
-                            recipient: inputData.number
-                        }),
-                        headers:{
-                            'Content-Type': "application/json"
-                        }
-                    });
-                    const json = await promise.json()
-                    setData([...data, json.payload])
+                    await api.messages.postMessage(inputData, data, setData)
                     setIsLoading(false)
                     localStorage.setItem('loadingMessage',  JSON.stringify(false))
                     setLocalStorageIsLoading(JSON.parse(localStorage.getItem('loadingMessage')))
@@ -80,7 +53,7 @@ const MessagesPage = () => {
         <section className="mainContent">
             <h1>CHATBOT WHATSAPP</h1>
             <InputMessages callback={inputChange} inputData={inputData}/>
-            <Button callback={changeState} body={'enviar'}/>
+            <Button callback={changeState} params={[]} body={'enviar'}/>
             <h2>ultimos mensajes enviados:</h2>
             <ItemListMessages data={data} setIsLoadingMessages={setLocalStorageIsLoading} isLoadingMessages={localStorageIsLoading} isLoading={isLoading} setIsLoading={setIsLoading}/>
         </section>
@@ -88,6 +61,5 @@ const MessagesPage = () => {
     )
 }
 
-//hay que arreglar que, si un mensaje esta cargando y yo me voy de la pagina cuando vuelva siga cargando
 
 export default MessagesPage

f353e55870741024a6bd20d34e41f89464ffe72b
CandelaMenaBisignano07 <136282696+CandelaMenaBisignano07@users.noreply.github.com>
2024-10-21 22:37:27 -0300

    refactor and fix: unused url constant deleted, imported api constant and changed destroyClient and fetchContacts functions locations to src/lib/api/api.js

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,77 +1,25 @@
-import React, { useEffect, useState, useRef, useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import ItemListContactsSideBar from '../itemLists/ItemListContactsSideBar'
 import { ClientContext } from '../context/ClientContext'
 import { Link, useNavigate } from 'react-router-dom'
 import Button from './Button'
+import { api } from '../lib/api/api'
 const SideBar = () => {
     const [contacts, setContacts] = useState([])
     const [contactsUptade, setContactsUptade] = useState([])
     const [input, setInput] = useState('')
     const {socket, setError} = useContext(ClientContext)
-    const URL = 'http://localhost:8080/api/';
     const navigate = useNavigate();
-    const destroyClient = async()=>{
-        try {
-            const clientDestroy = await fetch(`${URL}contacts`, {
-                method:'delete',
-                headers:{
-                    'Content-type': 'application/json'
-                }
-            })
-
-            if(clientDestroy.status != 200){
-                const {error: errorMessage} = await clientDestroy.json();
-                setError({code: clientDestroy.status, message: errorMessage})
-                return navigate(`/error/${clientDestroy.status}`)
-            }
-            localStorage.setItem('client', "")
-            return navigate('/loggedOut')
-        } catch (error) {
-            if(error.message === 'signal is aborted without reason'){
-                return
-            }  
-            else console.log(error.message)
-        }
-    }
     useEffect(()=>{
         const abortController = new AbortController()
         const signal = abortController.signal;
-        const fetchContacts = async()=>{
-            console.log('jaa')
-            try {
-                const contactsFetch = await fetch(`${URL}contacts`, {
-                    method:'GET',
-                    headers:{
-                        'Content-Type': "application/json"
-                    },
-                    signal:signal
-                })
-                console.log(contactsFetch.status)
-                if(contactsFetch.status != 200){
-                    const {error: errorMessage} = await contactsFetch.json();
-                    setError({code: contactsFetch.status, message: errorMessage})
-                    return navigate(`/error/${contactsFetch.status}`)
-                }
-                const {payload:payload2} = await contactsFetch.json();
-                console.log(payload2, 'hola')
-                const mappedContacts = payload2.map((c)=> {return{id:c.id, name:c.name}})
-                setContacts(mappedContacts);
-                setContactsUptade(mappedContacts);
-                console.log('setted')
-            } catch (error) {
-                if(error.message === 'signal is aborted without reason'){
-                    return
-                }  
-                else console.log(error.message)
-            }
-        };
-        fetchContacts()
+        api.contacts.fetchContacts(setError, navigate, signal, setContacts, setContactsUptade);
         const recievedEvent = socket.on('messageRecieved', ()=>{
-            fetchContacts()
+            api.contacts.fetchContacts(setError, navigate, signal, setContacts, setContactsUptade);
         })
 
         const sendedEvent = socket.on('messageSended', ()=>{
-            fetchContacts()
+            api.contacts.fetchContacts(setError, navigate, signal, setContacts, setContactsUptade);
         })
         return ()=>{
             socket.off('messageRecieved', recievedEvent)
@@ -100,7 +48,7 @@ const SideBar = () => {
             <nav>
                 <div>
                     <Link to='/home' className='homeAnchor'>inicio</Link>
-                    <Button body={'log out'} callback={destroyClient}/>
+                    <Button body={'log out'} callback={api.contacts.destroyClient} params={[setError, navigate, socket]}/>
                 </div>
                 <div className="inputContainer">
                     <input value={input} type="search" placeholder="buscar" onChange={(e)=>searchContacts(e.currentTarget.value)}/>
